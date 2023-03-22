bf3894416a00493f2d539d775afac1b517379432
Joyce928 <92929006+Joyce928@users.noreply.github.com>
2023-03-23 02:18:57 +1100

    Finished CSS Part

--- js/todo.js
+++ js/todo.js
@@ -23,7 +23,7 @@ function paintToDo(newTodo) {
   const span = document.createElement("span");
   span.innerText = newTodo.text;
   const button = document.createElement("button");
-  button.innerText = "✖";
+  button.innerText = " 🔲";
   button.addEventListener("click", deleteTodo);
   li.appendChild(span);
   li.appendChild(button);
